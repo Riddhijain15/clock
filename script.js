01fb420501ad94b7e9fd95aa39aebdf55d89d2cd
Riddhijain15 <142935417+Riddhijain15@users.noreply.github.com>
2024-08-09 09:51:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let element=document.getElementById('time')
+setInterval(() =>{
+let date= new Date()
+// let hours= 
+// let minutes=
+// let seconds=
+// console.log(hours,minutes,seconds)
+let time= `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+element.innerHTML=time
+
+},1000)
